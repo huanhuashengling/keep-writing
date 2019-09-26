c3c106d9f8d472acb8f1580e0c132e96cb5d8690
ywj <shengling_2005@163.com>
2019-09-26 12:24:58 +0800

    remove some columns don’t need now

--- resources/views/school/keep-record/index.blade.php
+++ resources/views/school/keep-record/index.blade.php
@@ -24,42 +24,42 @@
     <table id="keep-report" class="table table-condensed table-responsive">
         <thead>
             <tr>
-                <th data-field="" checkbox="true" rowspan="2">
+                <th data-field="" checkbox="true" rowspan="1">
 
                 </th>
-                <th data-field="" rowspan="2">
+                <th data-field="" rowspan="1">
                     序号
                 </th>
-                <th data-field="username" data-sortable="true" rowspan="2">
+                <th data-field="username" data-sortable="true" rowspan="1">
                     教师姓名
                 </th>
-                <th data-field="ageSection" data-sortable="true" rowspan="2">
+                <th data-field="ageSection" data-sortable="true" rowspan="1">
                     年龄
                 </th>
-                <th colspan="4">钢笔字</th>
-                <th colspan="4">粉笔字</th>
-                <th colspan="4">毛笔字</th>
-                <th data-field="allScoreCount" data-sortable="true" rowspan="2">
+                <th colspan="1">钢笔字打卡数</th>
+                <th colspan="1">粉笔字打卡数</th>
+                <th colspan="1">毛笔字打卡数</th>
+                <th data-field="allScoreCount" data-sortable="true" rowspan="1">
                     总分
                 </th>
-                <th data-field="isFormal" data-sortable="true"  rowspan="2" data-formatter="isFormalCol">
+                <th data-field="isFormal" data-sortable="true"  rowspan="1" data-formatter="isFormalCol">
                     正式标志
                 </th>
             </tr>
-            <tr>
-              <th data-field="penPostNum" data-sortable="true">打卡数</th>
-              <th data-field="penStarNum" data-sortable="true">星数</th>
-              <th data-field="penMarkNum">点赞</th>
-              <th data-field="penScore" data-sortable="true">分数</th>
-              <th data-field="chalkPostNum" data-sortable="true">打卡数</th>
-              <th data-field="chalkStarNum" data-sortable="true">星数</th>
-              <th data-field="chalkMarkNum">点赞</th>
-              <th data-field="chalkScore" data-sortable="true">分数</th>
-              <th data-field="brushPostNum" data-sortable="true">打卡数</th>
-              <th data-field="brushStarNum" data-sortable="true">星数</th>
-              <th data-field="brushMarkNum">点赞</th>
-              <th data-field="brushScore" data-sortable="true">分数</th>
-            </tr>
+            <!-- <tr> -->
+              <!-- <th data-field="penPostNum" data-sortable="true">打卡数</th> -->
+              <!-- <th data-field="penStarNum" data-sortable="true">星数</th> -->
+              <!-- <th data-field="penMarkNum">点赞</th> -->
+              <!-- <th data-field="penScore" data-sortable="true">分数</th> -->
+              <!-- <th data-field="chalkPostNum" data-sortable="true">打卡数</th> -->
+              <!-- <th data-field="chalkStarNum" data-sortable="true">星数</th> -->
+              <!-- <th data-field="chalkMarkNum">点赞</th> -->
+              <!-- <th data-field="chalkScore" data-sortable="true">分数</th> -->
+              <!-- <th data-field="brushPostNum" data-sortable="true">打卡数</th> -->
+              <!-- <th data-field="brushStarNum" data-sortable="true">星数</th> -->
+              <!-- <th data-field="brushMarkNum">点赞</th> -->
+              <!-- <th data-field="brushScore" data-sortable="true">分数</th> -->
+            <!-- </tr> -->
         </thead>
     </table>
     
